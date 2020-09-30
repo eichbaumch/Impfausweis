b645870299928178b4031a810d5601004b1784b3
Sophia <Sophia.Parpoulas@hs-furtwangen.de>
2020-09-30 14:00:23 +0200

    Initial sharing of project

---
+++ Patient.java
@@ -0,0 +1,20 @@
+
+/**
+ * Beschreiben Sie hier die Klasse Patient.
+ * 
+ * @author (Ihr Name) 
+ * @version (eine Versionsnummer oder ein Datum)
+ */
+public class Patient
+{
+    // Instanzvariablen - ersetzen Sie das folgende Beispiel mit Ihren Variablen
+    private String Name;
+    private 
+
+    /**
+     * Konstruktor für Objekte der Klasse Patient
+     */
+    public Patient()
+    {
+       
+}
